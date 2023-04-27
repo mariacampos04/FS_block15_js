4b20fd4b7e18d5f7ac2515cbb6b8406e45dbb0a6
Maria Campos <campos.maria04@outlook.com>
2023-04-26 19:47:14 -0500

    Final commit with 4 parts complete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//original customer object
 let customer = {
     firstName: "jake",
     lastName: "smith",
@@ -9,3 +10,34 @@ let customer = {
     favoriteStore: "Target",
     firstVisit: false
 }
+
+//print out of original object
+console.log(customer);
+
+//Modifying email, phone, zip code, and favorite flavors using bracket notation
+customer["email"] = "Jak3Smith1992@email.com";
+customer["phone"] = 3195551234;
+customer["zipCode"] = "63132";
+customer["favoriteFlavors"] = ["coffee", "strawberry", "match"];
+
+//print out of above modifications
+console.log(customer);
+
+//Deleting zip code and favorite store
+delete customer.zipCode;
+delete customer.favoriteStore;
+
+//print out of above modifications
+console.log(customer);
+
+//Adding toppings, future flavors, and purchase cost using dot notation
+customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+customer.futureFlavors = ["mango"];
+customer.todaysPurchaseCost = 5.32;
+
+//printing out the keys in customer object
+for(let key in customer){
+    console.log(key);
+}
+
+
